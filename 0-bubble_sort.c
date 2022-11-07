aa63760955798e2dcf249c3d864fa5d7a31e208a
root <yohannesmekonen091355@gmail.com>
2022-11-07 08:46:17 -0800

    ts

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,42 +1,47 @@
 #include "sort.h"
 
 /**
-*swap - swaps two integers in an array
-*@xp:first int
-*@yp:second int
-*Return:void
-*/
-void swap(int *xp, int *yp)
+ *swap - swaps two integers in an array
+ *@a:first int
+ *@b:second int
+ *Return:void
+ */
+void swap(int *a, int *b)
 {
-	int temp = *xp;
-	*xp = *yp;
-	*yp = temp;
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 /**
-*bubble_sort - sorts a list using the bubble sort algorithm
-*@array:pointer to array
-*@size:size of array
-*Return:void
-*/
+ *bubble_sort - sorts a list using the bubble sort algorithm
+ *@array:pointer to array
+ *@size:size of array
+ *Return:void
+ */
 void bubble_sort(int *array, size_t size)
 {
-	int new_size = size;
-	bool swapped;
+	size_t i, len;
+	unsigned int  test = 0;
 
-	while (new_size >= 0)
+	len = size;
+	if (array == NULL || size < 2)
+		return;
+	while (test == 0)
 	{
-	swapped = false;
-	for (int i = 0; i < size - 1; i++)
-	{
-	if (array[i] > array[i + 1])
-	{
-	swap(&array[i + 1], &array[i]);
-	print_array(array, size);
-	swapped = true;
-}
-}
-	if (swapped == false)
-	break;
-	new_size -= 1;
+		test = 1;
+		for (i = 0; i < len - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap(array + i, array + i + 1);
+				print_array(array, size);
+				test = 0;
+			}
+		}
+/*decrementing len coz every time a value is pushed to end, the */
+/*values are sorted at the end, reducing length we need to sort*/
+		len--;
 	}
 }
